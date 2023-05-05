06a710d8bf823b62ae605710e7462722f3db4e72
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2023-05-04 23:56:36 -0300

    feat: add Grid System

--- codigo/frontend/pages/index.tsx
+++ codigo/frontend/pages/index.tsx
@@ -1,5 +1,14 @@
+import { Col, Row } from "react-styled-flexboxgrid";
+
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+      <Row>
+        <Col xs={12} md={6}>
+          Item 1
+        </Col>
+        <Col xs={12} md={6}>
+          Item 2
+        </Col>
+      </Row>
+  );
 }
