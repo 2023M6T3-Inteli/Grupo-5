569a9b7023cfc20b692a1a8b362608de077e715f
Lívia Bonotto <liviabonotto@gmail.com>
2023-05-10 17:51:55 -0300

    first version

---
+++ codigo/frontend/components/createPost/index.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { useState } from "react";
+import styles from "./styles.module.scss";
+import { Col, Grid, Row } from "react-styled-flexboxgrid";
+
+
+
+
+
+
+
+
+const CreatePost: React.FC = () => {
+    const [text, setText] = useState("");
+    const maxLength = 500;
+
+    const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(event.target.value);
+    };
+
+    const charactersUsed = text.length;
+
+    // const posts = [
+    //     {
+    //         title: 'Post 1',
+
+    //     }
+    // ]
+
+    // const createPost = () => {
+    //     posts.push({
+    //         title: 'Post 2',
+
+    //     })
+    // }
+
+  return (
+    <div className={styles.createPost}>
+      <form>
+        <Grid>
+          <Row className={styles.row} center="xs">
+            <h2>New Post</h2>
+            <p className={styles.subtitle}>
+              Share your recommendations with the community!
+            </p>
+          </Row>
+          <Row className={styles.row}>
+            <Col xs={12}>
+              <div className={styles.inputGroup}>
+                <div className={styles.postTitleCounter}>
+                    <label>Post Content</label>
+                    <h5>{charactersUsed}/{maxLength}</h5>
+                </div>
+                <textarea
+                  placeholder="Check out this link!"
+                    required
+                  onChange={handleChange}
+                  maxLength={maxLength}
+                />
+              </div>
+            </Col>
+          </Row>
+          <Row className={styles.row}>
+            <Col xs={12}>
+              <div className={styles.inputGroup}>
+                <label>Add Image</label>
+                <input
+                  className={styles.fileUpload}
+                  type="file"
+                  aria-required="true"
+                />
+              </div>
+            </Col>
+          </Row>
+          <Row className={styles.row}>
+            <Col xs={12}>
+              <div className={styles.inputGroup}>
+                <label>Add Tags</label>
+                <input
+                  type="text"
+                  placeholder="IA, Phyton, Business..."
+                  required
+                />
+              </div>
+            </Col>
+          </Row>
+          <Row className={styles.row}>
+            <Col xs={12}>
+              <div className={styles.inputGroup}>
+                <input
+                  className={styles.createBtn}
+                  type="submit"
+                  value="Create Post"
+                />
+              </div>
+            </Col>
+          </Row>
+        </Grid>
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;
--- codigo/frontend/pages/index.tsx
+++ codigo/frontend/pages/index.tsx
@@ -1,17 +1,10 @@
 import React from 'react'
-import { Col, Row } from 'react-styled-flexboxgrid'
-import styles from './onboarding/styles.module.scss'
+import CreatePost from '@/components/createPost'
 
-const Home = () => {
+
+const Home: React.FC = () => {
   return (
-    <Row className={styles.container}>
-      <Col xs={12} md={6} className={styles.item}>
-        Item 1
-      </Col>
-      <Col xs={12} md={6}>
-        Item 2
-      </Col>
-    </Row>
+    <CreatePost/>
   )
 }
 
