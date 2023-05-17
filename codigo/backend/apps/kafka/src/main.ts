30a3a477d33f7623670fcf2daed027b2fc035981
Marcos Florencio <marcosaurelio.adm@gmail.com>
2023-05-17 08:40:05 -0300

    kafka first steps

--- codigo/backend/apps/kafka/src/main.ts
+++ codigo/backend/apps/kafka/src/main.ts
@@ -1,12 +1,27 @@
 /** nestjs */
 import { NestFactory } from "@nestjs/core";
+import { Transport } from "@nestjs/microservices";
+import { NestMicroserviceOptions } from "@nestjs/common/interfaces/microservices/nest-microservice-options.interface";
 
 /** modules */
 import { KafkaModule } from "./kafka.module";
 ////////////////////////////////////////////////////////////////////////////////
 
-async function bootstrap() {
-  const app = await NestFactory.create(KafkaModule);
-  await app.listen(3000);
-}
-bootstrap();
+/** bootstrap project */
+(async function () {
+  /** instantiate new microservice */
+  const app = await NestFactory.createMicroservice(KafkaModule, {
+    /** microservice options */
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: "kafka",
+        brokers: ["localhost:9092"],
+      },
+      consumer: {
+        groupId: "kafka-consumer",
+      },
+      topic: "kafka-topic",
+    },
+  });
+})();
