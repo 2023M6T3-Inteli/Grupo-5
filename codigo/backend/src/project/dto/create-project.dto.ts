12dd56d06d2a5cd60d5ad8e6bdecb3e872ddbe12
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2023-05-31 00:27:05 -0300

    feat: add create project

--- codigo/backend/src/project/dto/create-project.dto.ts
+++ codigo/backend/src/project/dto/create-project.dto.ts
@@ -4,6 +4,28 @@ import { IsString } from "class-validator";
 export class CreateProjectDto {
   @IsString()
   name: string;
-  @IsString({ each: true })
-  tags: string[];
+
+  @IsString()
+  description: string;
+
+  @IsString()
+  tags: string;
+
+  @IsString()
+  roles: string;
+
+  @IsString()
+  start: Date;
+
+  @IsString()
+  end: Date;
+
+  @IsString()
+  endSubscription: Date;
+
+  @IsString()
+  badge: string;
+
+  @IsString()
+  coleaderId?: string;
 }
--- codigo/backend/src/project/project.controller.ts
+++ codigo/backend/src/project/project.controller.ts
@@ -7,6 +7,8 @@ import {
   Patch,
   Param,
   Delete,
+  Req,
+  UseGuards,
 } from "@nestjs/common";
 
 /** providers */
@@ -15,6 +17,7 @@ import { CreateProjectDto } from "./dto/create-project.dto";
 import { UpdateProjectDto } from "./dto/update-project.dto";
 import { HttpService } from "@nestjs/axios";
 import { firstValueFrom } from "rxjs";
+import { AuthGuard } from "src/user/guards/auth.guard";
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -26,40 +29,47 @@ export class ProjectController {
     
   }
 
+  @UseGuards(AuthGuard)
   @NestPost()
-  async create(@Body() createProjectDto: CreateProjectDto, config: any) {
-    // const { data } = await firstValueFrom(
-    //   this.httpService.post("http://localhost:3001/Project/Create", createProjectDto, config)
-    // );
-    // return data;
-    this.projects.push({
-      ...createProjectDto,
-      id: this.projects.length + 1,
-      userId: 1,
-    });
-    return this.projects[this.projects.length - 1];
-  }
-
-  @Get()
-  findAll() {
-    return this.projects;
-  }
-
-  @Get(":id")
-  findOne(@Param("id") id: string) {
-    return this.projects.find((project) => project.id === +id);
-  }
-
-  @Patch(":id")
-  update(@Param("id") id: string, @Body() updateProjectDto: UpdateProjectDto) {
-    this.projects = this.projects.map((project) =>
-      project.id === +id ? { ...project, ...updateProjectDto } : project
+  async create(@Req() req, @Body() createProjectDto: CreateProjectDto) {
+    const token = req.headers.authorization
+    const config = {
+      headers: {
+        "Authorization": token
+      }
+    }
+    const { data } = await firstValueFrom(
+      this.httpService.post("http://localhost:3001/Project/Create", createProjectDto, config)
     );
-    return this.projects.find((project) => project.id === +id);
+    return data;
+    // this.projects.push({
+    //   ...createProjectDto,
+    //   id: this.projects.length + 1,
+    //   userId: 1,
+    // });
+    // return this.projects[this.projects.length - 1];
   }
 
-  @Delete(":id")
-  remove(@Param("id") id: string) {
-    this.projects = this.projects.filter((project) => project.id !== +id);
-  }
+  // @Get()
+  // findAll() {
+  //   return this.projects;
+  // }
+
+  // @Get(":id")
+  // findOne(@Param("id") id: string) {
+  //   return this.projects.find((project) => project.id === +id);
+  // }
+
+  // @Patch(":id")
+  // update(@Param("id") id: string, @Body() updateProjectDto: UpdateProjectDto) {
+  //   this.projects = this.projects.map((project) =>
+  //     project.id === +id ? { ...project, ...updateProjectDto } : project
+  //   );
+  //   return this.projects.find((project) => project.id === +id);
+  // }
+
+  // @Delete(":id")
+  // remove(@Param("id") id: string) {
+  //   this.projects = this.projects.filter((project) => project.id !== +id);
+  // }
 }
