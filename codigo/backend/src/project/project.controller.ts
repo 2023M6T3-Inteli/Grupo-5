38d6e87803681bd7f09540f94dbaac1528e0dd33
Delistoianov <joao.gonzalez@sou.inteli.edu.br>
2023-06-02 16:16:32 -0300

    feat: update integração legado

--- codigo/backend/src/project/project.controller.ts
+++ codigo/backend/src/project/project.controller.ts
@@ -11,6 +11,7 @@ import {
   UseGuards,
   Headers,
   Put,
+  Header,
 } from "@nestjs/common";
 
 /** providers */
@@ -75,6 +76,66 @@ export class ProjectController {
     }
     const { data } = await firstValueFrom(
       this.httpService.delete(`http://localhost:3001/Project/delete/${projectId}`, config)
+    );
+    return data;
+  }
+
+  @UseGuards(AuthGuard)
+  @Get("/allProjects")
+  async getAll(@Headers() headers) {
+    const token = headers.authorization
+    const config = {
+      headers: {
+        "Authorization": token
+      }
+    }
+    const { data } = await firstValueFrom(
+      this.httpService.get(`http://localhost:3001/Project/findAll`, config)
+    );
+    return data;
+  }
+
+  @UseGuards(AuthGuard)
+  @Get("/:projectId")
+  async getOne(@Param("projectId") projectId, @Headers() Headers) {
+    const token = Headers.authorization
+    const config = {
+      headers: {
+        "Authorization": token
+      }
+    }
+    const { data } = await firstValueFrom(
+      this.httpService.get(`http://localhost:3001/Project/findByID/${projectId}`, config)
+    );
+    return data;
+  }
+
+  @UseGuards(AuthGuard)
+  @NestPost("/filterProjects")
+  async filter(@Headers() headers, @Body() projectData: any) {
+    const token = headers.authorization
+    const config = {
+      headers: {
+        "Authorization": token
+      }
+    }
+    const { data } = await firstValueFrom(
+      this.httpService.post(`http://localhost:3001/Project/filter`, projectData, config)
+    );
+    return data;
+  }
+
+  @UseGuards(AuthGuard)
+  @Put("/finalize/:projectId")
+  async finalize(@Param("projectId") projectId, @Req() req: any) {
+    const token = req.headers.authorization
+    const config = {
+      headers: {
+        "Authorization": token
+      }
+    }
+    const { data } = await firstValueFrom(
+      this.httpService.put(`http://localhost:3001/Project/finalize/${projectId}`, {}, config)
     );
     return data;
   }
