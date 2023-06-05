dca3392ceace991117b2358119f2d61432071c27
Delistoianov <joao.gonzalez@sou.inteli.edu.br>
2023-06-05 17:03:30 -0300

    feat: add project service

--- codigo/backend/src/project/project.module.ts
+++ codigo/backend/src/project/project.module.ts
@@ -4,10 +4,12 @@ import { Module } from "@nestjs/common";
 /** controllers */
 import { ProjectController } from "./project.controller";
 import { HttpModule } from "@nestjs/axios";
+import { ProjectService } from "./project.service";
 ////////////////////////////////////////////////////////////////////////////////
 
 @Module({
-    imports: [HttpModule],
+  imports: [HttpModule],
   controllers: [ProjectController],
+  providers: [ProjectService]
 })
-export class ProjectModule {}
+export class ProjectModule { }
